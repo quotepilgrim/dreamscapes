06d3c846373c218463e4d59e027f5f53786c31e5
Quote Pilgrim <caio.psv@gmail.com>
2024-09-17 23:42:28 -0300

    added basic versioning system

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 cd dreamscapes
 unzip -fo ../info.ipk3
-zip -rFS ../dist/dreamscapes.ipk3 .
+output=$(zip -rFS ../dist/dreamscapes.ipk3 .)
+
+echo $output
+
+version_file="version"
+
+if [[ "$output" != *"Archive is current"* ]]; then
+    if [ -f "$version_file" ]; then
+        version=$(<"$version_file")
+        new_version=$((version + 1))
+        echo "$new_version" > "$version_file"
+        echo "Version updated to $new_version"
+        zip -rFS ../dist/dreamscapes.ipk3 .
+    else
+        echo "Version file does not exist."
+    fi
+fi
